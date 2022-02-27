fb3f385d3e1b09bb736b3e941c1dfc46981c6147
Erik Kvalvik <erik.kvalvik@no.experis.com>
2022-02-27 13:27:56 +0100

    added api for checking and creating users

---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { loginUser } from '../../api/user'
+
+const usernameConfig = {
+    required: true,
+    minLength: 3
+}
+
+const LoginForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm()
+
+    const [ loading, setLoading ] = useState(false)
+
+    const onSubmit = async ({ username }) => {
+        setLoading(true)
+        const [error, user] = await loginUser(username)
+        console.log('Error: ', error)
+        console.log('User: ', user)
+        setLoading(false)
+    }
+    
+    console.log(errors)
+
+    const errorMessage = (() => {
+        if ( !errors.username ){
+            return null
+        }
+        if(errors.username.type === 'required'){
+            return <span>Username is required</span>
+        }
+        if(errors.username.type === 'minLength'){
+            return <span>Username is too short (min 3 characters)</span>
+        }
+    })()
+
+    return(
+        <>
+            <h2>Whats your name?</h2>
+            <form onSubmit={ handleSubmit(onSubmit) }>
+                <fieldset>
+                    <label htmlFor="username">Username: </label>
+                    <input 
+                        type="text" 
+                        placeholder="What is your name?"
+                        { ...register("username", usernameConfig) } />
+                       {errorMessage}
+                </fieldset>
+
+                <button type="submit" disabled={ loading }>Continue</button>
+
+                { loading &&  <p>Logging in...</p>}
+            </form>
+        </>
+    )
+}
+export default LoginForm
